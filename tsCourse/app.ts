2f6eab722eefc282b9007b8296f8fcbe7562b6a7
Marat Baikin <3371335@gmail.com>
2023-08-05 15:01:55 +0300

    Update app.ts

--- tsCourse/app.ts
+++ tsCourse/app.ts
@@ -9,3 +9,17 @@ function add (num1 :number, num2: number) {
 button!.addEventListener('click', function () {
     console.log(add(+input1.value, +input2.value))
 })
+
+let arrObj: { name: string }[];
+arrObj = [{ name: "Marat" }, { name: 2 }, "first", 3]
+
+const obj: {} = {};
+const obj1: object = {};
+// ? Указывает, что поле не обязательное
+const obj2: { name?: string } = {};
+obj2.name = "Marat"
+const obj3: {name: string, age: number} = {name: "Marat", age: 34};
+const obj4 = {
+    name: "Marat",
+    age: 34,
+}
