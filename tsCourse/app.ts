5fb69f9789ec77a6049838d9d8ee4aa760d9a185
Marat Baikin <3371335@gmail.com>
2023-08-14 17:51:14 +0300

    Update app.ts

--- tsCourse/app.ts
+++ tsCourse/app.ts
@@ -119,7 +119,7 @@ function customError(): never {
 
 // 9. Создайте свой тип данных на основе имеющихся данных.
 
-type pageInfo = {
+type PageInfo = {
   title: string
   likes: number
   accounts: string []
@@ -130,20 +130,20 @@ type pageInfo = {
   }
 }
 
-// const page1 = {
-//   title: 'The awesome page',
-//   likes: 100,
-//   accounts: ['Max', 'Anton', 'Nikita'],
-//   status: 'open',
-//   details: {
-//     createAt: new Date('2021-01-01'),
-//     updateAt: new Date('2021-05-01'),
-//   }
-// }
+const page1: PageInfo = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: new Date('2021-01-01'),
+    updateAt: new Date('2021-05-01'),
+  }
+}
 
-// const page2 = {
-//   title: 'Python or Js',
-//   likes: 5,
-//   accounts: ['Alex'],
-//   status: 'close',
-// }
+const page2: PageInfo = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+}
