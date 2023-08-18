f769abc650526e5d1c05e3f226ea4f692fb4a012
Marat Baikin <3371335@gmail.com>
2023-08-18 17:53:16 +0300

    upd

--- tsCourse/src/classTask.ts
+++ tsCourse/src/classTask.ts
@@ -4,8 +4,40 @@ class House {
     constructor (n: string){
     this.street = n
     }
+
+    showAddress(this: House) {
+        console.log(`Adress: ${this.street}`)
+        return this.street
+    }
 }
 
 const house = new House('Svobodi')
+const houseCopy = {street: "Tobolska", showAddress: house.showAddress}
 
 console.log(house)
+console.log(house.showAddress())
+console.log(houseCopy.showAddress())
+
+
+// public - это как все свойства и методы в js можно вызывать где угодно
+// private - нельзя вызвать извне экземпляра не наследуется
+// protected - нельзя вызвать извне экземпляра но наследуется
+
+class A {
+    protected someProperty = 'str'
+}
+
+class B extends A {
+    showProperty() {
+      console.log(this.someProperty)  
+    }
+    
+}
+
+const a = new A()
+const b = new B()
+
+// a.someProperty
+// B.someProperty
+
+b.showProperty()
