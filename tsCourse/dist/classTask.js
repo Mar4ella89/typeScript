32fdd8ec7af2a21faba1bb4092c8186fae142848
Marat Baikin <3371335@gmail.com>
2023-08-18 16:32:15 +0300

    upd

---
+++ tsCourse/dist/classTask.js
@@ -0,0 +1,9 @@
+"use strict";
+class Home {
+    constructor(n) {
+        this.street = n;
+    }
+}
+const house = new Home('Svobodi');
+console.log(house);
+//# sourceMappingURL=classTask.js.map
---
+++ tsCourse/src/classTask.ts
@@ -0,0 +1,11 @@
+class House {
+    street: string
+
+    constructor (n: string){
+    this.street = n
+    }
+}
+
+const house = new House('Svobodi')
+
+console.log(house)
