a9028da7ad6820c733fdbc733cc67a31f909c4b7
Marat Baikin <3371335@gmail.com>
2023-08-21 17:55:35 +0300

    upd

--- tsCourse/dist/classTask.js
+++ tsCourse/dist/classTask.js
@@ -1,28 +1,70 @@
 "use strict";
 class House {
-    constructor(n) {
+    constructor(type, street) {
+        this.type = type;
+        this.street = street;
+        // private street: string
         this.tenants = [];
-        this.street = n;
     }
+    // constructor (n: string){
+    // this.street = n
+    // }
     showAddress() {
         console.log(`Adress: ${this.street}`);
         return this.street;
     }
+    showType() {
+        console.log(`House Type: ${this.type}`);
+    }
     addTenant(tenant) {
         this.tenants.push(tenant);
     }
-    showTenant() {
+    showTenants() {
         console.log(this.tenants);
     }
 }
-const house = new House('Svobodi');
+class StoneHouse extends House {
+    constructor(street, generalTenant) {
+        super('stone', street); // Вызов родительского конструктора
+        // Добавляем владельца квартиры
+        this.chargeOfTheHouse = generalTenant;
+        this.addTenant(generalTenant);
+    }
+    showTenants() {
+        console.log(`General:  ${this.chargeOfTheHouse}`);
+        // Запускаем родительский метод showTenants();
+        super.showTenants();
+    }
+}
+const stoneHouse = new StoneHouse('Stone-world', 'Max');
+stoneHouse.addTenant('Anton');
+stoneHouse.addTenant('Nikita');
+stoneHouse.showTenants();
+stoneHouse.showType();
+stoneHouse.showAddress();
+// const house = new House('Svobodi')
 // const houseCopy = {street: "Tobolska", tenants: [], showAddress: house.showAddress}
-console.log(house);
-console.log(house.showAddress());
+// console.log(house)
+// console.log(house.showAddress())
 // console.log(houseCopy.showAddress())
-house.addTenant('Marat');
-house.addTenant('Anna');
-house.showTenant();
+// house.addTenant('Marat')
+// house.addTenant('Anna')
+// house.showTenant()
+// console.log(house.tenants.push('Nik'))
+// =======================
+// class House1 {
+//     constructor(private readonly type: string, private street: string) {
+//     }
+// }
+// class StoneHouse extends House1 {
+//     constructor (street: string) {
+//         super('stone', street)
+//     }
+// }
+// console.log(StoneHouse.street)
+// const stoneHouse = new StoneHouse('Stone-world');
+// console.log(stoneHouse.street)
+// =======================
 // public - это как все свойства и методы в js можно вызывать где угодно
 // private - нельзя вызвать извне экземпляра не наследуется
 // protected - нельзя вызвать извне экземпляра но наследуется
--- tsCourse/src/classTask.ts
+++ tsCourse/src/classTask.ts
@@ -1,14 +1,22 @@
 class House {
-    private street: string
-    private tenants:string[]=[]
-
-    constructor (n: string){
-    this.street = n
+    // private street: string
+    private tenants: string[] = []
+    
+    constructor(private readonly type: string, private street: string) {
+        
     }
+
+    // constructor (n: string){
+    // this.street = n
+    // }
 
    public showAddress (this: House) {
         console.log(`Adress: ${this.street}`)
         return this.street
+    }
+
+    public showType(this: House) {
+        console.log(`House Type: ${this.type}`)
     }
 
     public addTenant (tenant: string) {
@@ -16,23 +24,66 @@ class House {
     }
     
     
-    public showTenant() {
+    public showTenants() {
         console.log(this.tenants)
     }
 }
 
-const house = new House('Svobodi')
+class StoneHouse extends House {
+    private chargeOfTheHouse: string;
+    constructor(street: string, generalTenant: string) {
+        super('stone', street); // Вызов родительского конструктора
+        // Добавляем владельца квартиры
+    this.chargeOfTheHouse = generalTenant;
+    this.addTenant(generalTenant);
+    }
+
+    public showTenants() {
+        console.log(`General:  ${this.chargeOfTheHouse}`);
+        // Запускаем родительский метод showTenants();
+        super.showTenants();
+    }
+}
+
+const stoneHouse = new StoneHouse('Stone-world', 'Max');
+
+stoneHouse.addTenant('Anton');
+stoneHouse.addTenant('Nikita');
+
+stoneHouse.showTenants();
+stoneHouse.showType();
+stoneHouse.showAddress();
+
+// const house = new House('Svobodi')
 // const houseCopy = {street: "Tobolska", tenants: [], showAddress: house.showAddress}
 
-console.log(house)
-console.log(house.showAddress())
+// console.log(house)
+// console.log(house.showAddress())
 // console.log(houseCopy.showAddress())
 
-house.addTenant('Marat')
-house.addTenant('Anna')
-house.showTenant()
-console.log(house.tenants.push('Nik'))
+// house.addTenant('Marat')
+// house.addTenant('Anna')
+// house.showTenant()
+// console.log(house.tenants.push('Nik'))
 
+// =======================
+
+// class House1 {
+//     constructor(private readonly type: string, private street: string) {
+        
+//     }
+// }
+
+// class StoneHouse extends House1 {
+//     constructor (street: string) {
+//         super('stone', street)
+//     }
+// }
+// console.log(StoneHouse.street)
+
+// const stoneHouse = new StoneHouse('Stone-world');
+// console.log(stoneHouse.street)
+// =======================
 
 // public - это как все свойства и методы в js можно вызывать где угодно
 // private - нельзя вызвать извне экземпляра не наследуется
@@ -46,7 +97,6 @@ class B extends A {
     showProperty() {
       console.log(this.someProperty)  
     }
-    
 }
 
 const a = new A()
