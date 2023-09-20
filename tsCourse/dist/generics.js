98b5d5f08fcc1a33977594d0c934f9bafd615e41
Marat Baikin <3371335@gmail.com>
2023-09-20 18:03:18 +0300

    upd

--- tsCourse/dist/generics.js
+++ tsCourse/dist/generics.js
@@ -1,4 +1,14 @@
 "use strict";
 let arrayG = [];
 arrayG = ['Marat', 34, '11'];
+// promise
+const promiseG = new Promise((resolve) => {
+    setInterval(() => {
+        resolve('Done!');
+    }, 1000);
+});
+promiseG.then(data => {
+    console.log(data);
+});
+// Generic function/method
 //# sourceMappingURL=generics.js.map
--- tsCourse/src/generics.ts
+++ tsCourse/src/generics.ts
@@ -1,2 +1,16 @@
 let arrayG: Array<string | number> = []
 arrayG = ['Marat', 34, '11']
+
+// promise
+
+const promiseG: Promise<string> = new Promise((resolve) => {
+    setInterval(() => {
+        resolve('Done!')
+    }, 1000)
+})
+
+promiseG.then(data => {
+    console.log(data)
+})
+
+// Generic function/method
