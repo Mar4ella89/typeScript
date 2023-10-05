58d50b4bb2bcfdbdcfe1eea2c382c71ef692dca9
Marat Baikin <3371335@gmail.com>
2023-10-05 17:06:01 +0300

    upd

--- tsCourse/dist/classTS.js
+++ tsCourse/dist/classTS.js
@@ -71,8 +71,27 @@ class HouseShort {
         return this.type;
     }
 }
-// 4 Readonly
+// 4 Readonly (похож на const)
 class HouseRO {
-    constructor() { }
+    constructor(type, tenants, address) {
+        this.type = type;
+        this.tenants = tenants;
+        this.address = address;
+    }
+    // changeType(type: string) {  // Будет ошибка, т.к. св-во type имеет readonly
+    //     this.type = type
+    // }
+    showType() {
+        this.type;
+    }
+    changeStreet(tenant) {
+        this.tenants.push(tenant); // Не будет ошибки, т.к. св-во tenants имеет readonly, но не запрещает изменять массив
+    }
 }
+const myHouseRO = new HouseRO('new', [], { street: 'Miru', number: 11 });
+// myHouseRO.address = {street: 'Mira', number: 14 } /*Будет ошибка, т.к. нельзя менять св-ва */
+// Но можно сделать так:
+console.log('number before change: ', myHouseRO.address.number);
+myHouseRO.address.number = 15;
+console.log('number after change: ', myHouseRO.address.number);
 //# sourceMappingURL=classTS.js.map
--- tsCourse/src/classTS.ts
+++ tsCourse/src/classTS.ts
@@ -98,16 +98,29 @@ class HouseShort {
     }
 }
 
-// 4 Readonly
+// 4 Readonly (похож на const)
 
 class HouseRO {
-    constructor(private readonly type: string, private readonly tenants: string[]) {}
+    constructor(private readonly type: string,
+        private readonly tenants: string[],
+        public readonly address: {street: string, number: number}) { }
 
-    // cgangeType(type: string) {  // Будет ошибка, т.к. св-во type имеет readonly
+    // changeType(type: string) {  // Будет ошибка, т.к. св-во type имеет readonly
     //     this.type = type
     // }
+    showType() {
+        this.type
+    }
 
     changeStreet(tenant: string) {
-        this.tenants.push('Marat') // Не будет ошибки, т.к. св-во tenants имеет readonly, но не запрещает изменять массив
+        this.tenants.push(tenant) // Не будет ошибки, т.к. св-во tenants имеет readonly, но не запрещает изменять массив
     }
 }
+
+const myHouseRO = new HouseRO('new', [], { street: 'Miru', number: 11 })
+
+// myHouseRO.address = {street: 'Mira', number: 14 } /*Будет ошибка, т.к. нельзя менять св-ва */
+// Но можно сделать так:
+console.log('number before change: ',myHouseRO.address.number)
+myHouseRO.address.number = 15
+console.log('number after change: ',myHouseRO.address.number)
