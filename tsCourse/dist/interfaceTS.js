a6e89109a22f8c15efa91f1409c0889b04b4964b
Marat Baikin <3371335@gmail.com>
2023-10-10 17:59:46 +0300

    upd

--- tsCourse/dist/interfaceTS.js
+++ tsCourse/dist/interfaceTS.js
@@ -7,4 +7,32 @@ const newPersonI = {
     lastName: 'Baikin'
 };
 greet(newPersonI);
+let user1;
+user1 = {
+    name: 'Marat',
+    age: 34,
+    greet(phrase) {
+        console.log(`${phrase} ${this.name}`);
+    }
+};
+user1.greet('Hi, i am');
+class Pilot2 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        if (this.age <= 25) {
+            throw 'You are really young!';
+        }
+    }
+    greet(phrase) {
+        console.log(`${phrase} ${this.name}`);
+    }
+    flyMessage() {
+        console.log('Plane is starting fly');
+    }
+    // Интерфейсы не ограничивают нас в расширении классов. Мы можем добавлять в класс новые методы и свойства.
+    setAutopilot() {
+        console.log('Autopilot enabled.');
+    }
+}
 //# sourceMappingURL=interfaceTS.js.map
--- tsCourse/src/interfaceTS.ts
+++ tsCourse/src/interfaceTS.ts
@@ -1,3 +1,4 @@
+// 1.
 interface InewPerson {
     firstName: string;
     lastName: string;
@@ -14,3 +15,58 @@ const newPersonI: InewPerson = {
 }
 
 greet(newPersonI)
+
+// 2. Интерфейсы объектов
+
+interface IPerson1 {
+    name: string;
+    age: number;
+
+    greet(phrase: string): void
+}
+
+let user1: IPerson1
+
+user1 = {
+    name: 'Marat',
+    age: 34,
+
+    greet(phrase) {
+        console.log(`${phrase} ${this.name}`)
+    }
+}
+
+user1.greet('Hi, i am')
+
+// 3. Интерфейсы классов
+
+interface IPerson2 {
+    name: string;
+    age: number;
+
+    greet(phrase: string):void
+}
+
+interface IPilot2 {
+    flyMessage(): void
+}
+
+class Pilot2 implements IPerson2, IPilot2 {
+    constructor(public name: string, public age: number) {
+        if (this.age <= 25) {
+            throw 'You are really young!'
+        }
+    }
+    greet(phrase: string): void {
+            console.log(`${phrase} ${this.name}`)
+    }
+    
+    flyMessage() {
+        console.log('Plane is starting fly')
+    }
+
+    // Интерфейсы не ограничивают нас в расширении классов. Мы можем добавлять в класс новые методы и свойства.
+    setAutopilot(): void {
+    console.log('Autopilot enabled.');
+  }
+}
