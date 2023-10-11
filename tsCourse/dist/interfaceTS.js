63720a5104b6bf82713dd2927087899daa6f5fea
Marat Baikin <3371335@gmail.com>
2023-10-11 18:01:46 +0300

    upd

--- tsCourse/dist/interfaceTS.js
+++ tsCourse/dist/interfaceTS.js
@@ -35,4 +35,50 @@ class Pilot2 {
         console.log('Autopilot enabled.');
     }
 }
+class Plane2 {
+    sitInPlane(pilot) {
+        this.pilot = pilot;
+    }
+}
+class Boeing2 extends Plane2 {
+    startEngine() {
+        if (!this.pilot) {
+            throw new Error('No pilot in cabin');
+        }
+        // Розігріваємо реактивні турбіни.
+        console.log('Запуск турбин');
+        this.pilot.flyMessage();
+        return true;
+    }
+}
+const boeing2 = new Boeing2();
+const pilot2 = new Pilot2('Anthony', 32);
+// Капітан вітає пасажирів на трапі.
+pilot2.greet('Вас вітає капітан корабля');
+// Займає місце пілота.
+boeing2.sitInPlane(pilot2);
+// Запускаємо двигуни.
+boeing2.startEngine();
+class Terrorist2 {
+    bluff(phrase) {
+        console.log(phrase);
+    }
+    flyMessage() {
+        console.log("Наші вимоги - 9 мільйонів.");
+    }
+}
+const boeingT = new Boeing2();
+const pilotT = new Terrorist2();
+// Капітан вітає пасажирів на трапі.
+pilotT.bluff('Ви захопили цей літак!');
+// Займає місце пілота.
+boeingT.sitInPlane(pilotT);
+// Запускаємо двигуни.
+boeingT.startEngine();
+const someUser = {
+    name: 'Marat'
+};
+console.log(someUser.name);
+// someUser.name = 'Mik'  Свойство досупно только для чтения
+// 5. Extending Interfaces
 //# sourceMappingURL=interfaceTS.js.map
--- tsCourse/src/interfaceTS.ts
+++ tsCourse/src/interfaceTS.ts
@@ -70,3 +70,75 @@ class Pilot2 implements IPerson2, IPilot2 {
     console.log('Autopilot enabled.');
   }
 }
+
+abstract class Plane2 {
+    protected pilot?: IPilot2
+
+    public sitInPlane(pilot: IPilot2): void{
+        this.pilot = pilot;
+    }
+
+    public abstract startEngine(): boolean;
+}
+
+class Boeing2 extends Plane2 {
+  public startEngine(): boolean {
+    if (!this.pilot) {
+      throw new Error('No pilot in cabin');
+    }
+    // Розігріваємо реактивні турбіни.
+    console.log('Запуск турбин');
+
+    this.pilot.flyMessage();
+    return true;
+  }
+}
+
+const boeing2 = new Boeing2();
+const pilot2 = new Pilot2('Anthony', 32);
+
+// Капітан вітає пасажирів на трапі.
+pilot2.greet('Вас вітає капітан корабля');
+
+// Займає місце пілота.
+boeing2.sitInPlane(pilot2);
+
+// Запускаємо двигуни.
+boeing2.startEngine();
+
+class Terrorist2 implements IPilot {
+  bluff(phrase: string): void {
+    console.log(phrase);
+  }
+
+  flyMessage(): void {
+    console.log("Наші вимоги - 9 мільйонів.");
+  }
+}
+
+const boeingT = new Boeing2();
+const pilotT = new Terrorist2();
+
+// Капітан вітає пасажирів на трапі.
+pilotT.bluff('Ви захопили цей літак!');
+
+// Займає місце пілота.
+boeingT.sitInPlane(pilotT);
+
+// Запускаємо двигуни.
+boeingT.startEngine();
+
+// 4. Readonly
+
+interface IReadonly {
+    readonly name: string
+}
+
+const someUser: IReadonly = {
+    name: 'Marat'
+}
+
+console.log(someUser.name)
+// someUser.name = 'Mik'  Свойство досупно только для чтения
+
+// 5. Extending Interfaces
